144a0d966d890b3bba5360b455e8fb4c78a01917
KacPerPrzepiorka <wrx90541@student.wroclaw.merito.pl>
2025-04-12 16:48:57 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -8,4 +8,23 @@ function srednia($oceny) {
 
 }
 
+function sredniaWazona($oceny, $wagi) {
+
+    $suma = 0;
+
+    $wagaSuma = 0;
+
+    for ($i = 0; $i < count($oceny); $i++) {
+
+        $suma += $oceny[$i] * $wagi[$i];
+
+        $wagaSuma += $wagi[$i];
+
+    }
+
+    return $suma / $wagaSuma;
+
+}
+
+
 ?>
